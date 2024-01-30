40b08e2caf51efee793ce975d12fa2a56e0571ba
= <=>
2024-01-30 18:44:35 +0300

    userService.init и событие window onload выведены из router.ts в index.ts
     Changes to be committed:
    	modified:   src/index.ts
    	modified:   src/router.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import "./icons";
 import Router from "./router";
 import { cartService } from "./services/cart.service";
+import { userService } from "./services/user.service";
 
-new Router();
-cartService.init();
+window.addEventListener('load', async e => {
+  e.preventDefault();
+  
+  await userService.init();
+  new Router();
+  cartService.init();
+});
 
 setTimeout(() => {
   document.body.classList.add("is__ready");
--- src/router.ts
+++ src/router.ts
@@ -4,8 +4,6 @@ import { homepageComp } from './modules/homepage/homepage';
 import { productDetailComp } from './modules/productDetail/productDetail';
 import { checkoutComp } from './modules/checkout/checkout';
 
-import { userService } from './services/user.service';
-
 const ROUTES = {
   '/': homepageComp,
   '/catalog': catalogComp,
@@ -20,14 +18,12 @@ export default class Router {
     // @ts-ignore
     this.$appRoot = document.querySelector('.js__root');
 
-    window.addEventListener('load', this.route.bind(this));
     window.addEventListener('hashchange', this.route.bind(this));
+
+    this.route();
   }
 
-  async route(e: any) {
-    e.preventDefault();
-
-    await userService.init();
+  async route() {
 
     // @ts-ignore
     const component = ROUTES[window.location.pathname] || notFoundComp;
